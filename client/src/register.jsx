2deaa43cc63bde70dfc3c437ac21107d0c4683e8
Mahmoud Fouad <mfapolice@gmail.com>
2023-04-12 23:22:05 +0200

    make a register page

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>hi</div>
-  )
-}
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto ">
+        <input
+          type="text"
+          placeholder="username"
+          className="block w-full p-2 rounded-sm mb-2 border"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="block w-full p-2 rounded-sm mb-2 border"
+        />
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
